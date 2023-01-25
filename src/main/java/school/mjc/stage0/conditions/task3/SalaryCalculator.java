fdbfa50c2c60f0fb93fb220322521787975e8d88
limbxbx <ilya.msd2003@gmail.com>
2023-01-25 12:15:50 +0200

    Tasks completed

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double taxes;
+        if (salary <= 10000) {
+            taxes = 0.15;
+        } else if (salary > 10000 && salary <= 20000) {
+            taxes = 0.18;
+        } else if (salary > 20000) {
+            taxes = 0.20;
+        } else {
+            System.out.println("wrong input!");
+            return;
+        }
+        System.out.println(salary - (salary * taxes));
     }
 }
